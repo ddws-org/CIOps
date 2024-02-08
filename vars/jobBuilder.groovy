b4a6c7c4fefc405a2cf28094eebec36ddf52a923
Gowtham-egov <112694085+Gowtham-egov@users.noreply.github.com>
2024-02-08 17:02:52 +0530

    Update jobBuilder.groovy

--- vars/jobBuilder.groovy
+++ vars/jobBuilder.groovy
@@ -93,7 +93,7 @@ spec:
                         description('') 
                         branch('')      
                         useRepository('')                     
-                        defaultValue('origin/naljal') 
+                        defaultValue('origin/master') 
                         branchFilter('.*')
                         tagFilter('*')
                         sortMode('ASCENDING_SMART')
