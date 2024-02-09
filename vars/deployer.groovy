d6a16fcbe6139e5e24757dc2cf958c4161436c38
Gowtham-egov <112694085+Gowtham-egov@users.noreply.github.com>
2024-02-09 09:47:14 +0530

    Update deployer.groovy

--- vars/deployer.groovy
+++ vars/deployer.groovy
@@ -48,7 +48,6 @@ spec:
                         export KUBECONFIG=/root/.kube/config
                         kubectl config get-contexts
                         kubectl config current-context
-                        cat /root/.kube/config
                         aws-iam-authenticator version
                         kubectl get nodes
                     """
